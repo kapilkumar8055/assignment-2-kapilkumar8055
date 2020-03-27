dbaddfeac1c925cf97e1f64f3ff4e5e437a07902
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-27 19:26:59 +0530

    create a method for in order traversal

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -7,4 +7,12 @@ import java.util.Queue;
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
     static Queue<TreeNode> queue;
+
+    public void inordertraversal(TreeNode root) {
+        if (root.getLeft() != null) {
+            inordertraversal(root.getLeft());
+            System.out.println(root.getData());
+            inordertraversal(root.getRight());
+        }
+    }
 }
