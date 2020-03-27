d44ba291095baf3564f99d0e3fc599862dfce2af
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-27 14:51:33 +0530

    create getter and setter

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -17,4 +17,27 @@ public class Node {
         this.next = null;
     }
 
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }
