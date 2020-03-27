0d0b03e7122d752bdf8193ee62943f43d438ce4c
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-27 14:43:37 +0530

    data variable is created for storing data

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -1,10 +1,11 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User: kapil kumar
  *  Date: 23-Mar-20
  *  Time: 7:13 PM
  */
 package problem3.node;
 
 public class Node {
+    private int data;
 }
