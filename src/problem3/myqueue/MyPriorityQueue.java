3f152de2c0c3d76958037ee33da91ca040e87404
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-27 15:06:21 +0530

    create front and rear variable

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -1,10 +1,14 @@
 /*
  *  Created by IntelliJ IDEA.
- *  User: Vaibhav
+ *  User: Kapil kumar
  *  Date: 23-Mar-20
- *  Time: 7:15 PM
+ *  Time: 7:25 PM
  */
 package problem3.myqueue;
 
+import problem3.node.Node;
+
 public class MyPriorityQueue {
+    private Node front;
+    private Node rear;
 }
