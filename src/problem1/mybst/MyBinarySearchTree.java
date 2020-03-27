98a6cf14c66a54f02c00bdcd5af00e9a336eba2c
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-27 19:22:25 +0530

    create constructor

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -1,10 +1,10 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
+import java.util.Queue;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree {
+    static Queue<TreeNode> queue;
 }
