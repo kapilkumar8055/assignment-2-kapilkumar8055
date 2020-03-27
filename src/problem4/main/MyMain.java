273652aae6e4773c5d8ceea5c265f0a40fb854e9
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-28 02:10:52 +0530

    insert the data in BST

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -1,6 +1,7 @@
 package problem4.main;
 
 import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
 import problem4.myqueue.MyQueue;
 
 // executable class
@@ -9,5 +10,16 @@ public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
         MyQueue myQueue = new MyQueue();
+        TreeNode root = new TreeNode(10);
+        myBinarySearchTree.insert(root, 20);
+        myBinarySearchTree.insert(root, 15);
+        myBinarySearchTree.insert(root, 18);
+        myBinarySearchTree.insert(root, 25);
+        myBinarySearchTree.insert(root, 30);
+        myBinarySearchTree.insert(root, 28);
+        myBinarySearchTree.insert(root, 16);
+        myBinarySearchTree.insert(root, 7);
+        myBinarySearchTree.insert(root, 3);
+        myBinarySearchTree.insert(root, 1);
     }
 }
