356a8c40c64e25efdc796ff4c196de24dc0886af
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-27 22:27:06 +0530

    insert data

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,6 +8,14 @@ public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
         TreeNode root = new TreeNode(20);
-        myBinarySearchTree.insert(root, 21);
+        myBinarySearchTree.insert(root, 15);
+        myBinarySearchTree.insert(root, 10);
+        myBinarySearchTree.insert(root, 12);
+        myBinarySearchTree.insert(root, 17);
+        myBinarySearchTree.insert(root, 25);
+        myBinarySearchTree.insert(root, 30);
+        myBinarySearchTree.insert(root, 5);
+        myBinarySearchTree.insert(root, 35);
+        myBinarySearchTree.insert(root, 32);
     }
 }
