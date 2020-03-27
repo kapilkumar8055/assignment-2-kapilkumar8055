5d2804eddf1de5b8b028b36db690e20053cb810c
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-27 22:25:14 +0530

    insert the data using insert method

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -2,10 +2,12 @@ package problem1.main;
 // executable class
 
 import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
 
 public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
-
+        TreeNode root = new TreeNode(20);
+        myBinarySearchTree.insert(root, 21);
     }
 }
