fbffdeb80d1bf34492d83756a9c314a401eca604
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-27 19:07:56 +0530

    create variables to store data and reference

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -1,10 +1,7 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
 package problem1.node;
 
 public class TreeNode {
+    private int data;
+    private TreeNode left;
+    private TreeNode right;
 }
