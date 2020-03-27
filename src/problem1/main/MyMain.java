b8fdcddd542ee5b8b21d2ee657611026aa39c6e0
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-27 22:22:42 +0530

    create object

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,8 +1,11 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
 public class MyMain {
     public static void main(String[] args) {
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
 
     }
 }
