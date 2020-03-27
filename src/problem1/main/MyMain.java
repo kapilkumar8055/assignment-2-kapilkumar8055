800d581217149f005e77de65c09a318591ca8065
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-27 22:21:34 +0530

    create main method

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -1,11 +1,8 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:17 PM
- */
 package problem1.main;
 // executable class
 
 public class MyMain {
+    public static void main(String[] args) {
+
+    }
 }
