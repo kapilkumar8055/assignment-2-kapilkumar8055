96a1254f4f2349e729e266032de4eb3ce9874c01
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-27 19:09:50 +0530

    create constructor

--- src/problem1/node/TreeNode.java
+++ src/problem1/node/TreeNode.java
@@ -4,4 +4,10 @@ public class TreeNode {
     private int data;
     private TreeNode left;
     private TreeNode right;
+
+    public TreeNode(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
 }
