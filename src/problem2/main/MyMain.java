93b3a288641b64f83c038e63821ba6e7776b1654
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-27 23:57:47 +0530

    create object of Traverse class

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -3,6 +3,6 @@ package problem2.main;
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
     public static void main(String[] args) {
-
+    Traverse traverse = new Traverse();
     }
 }
