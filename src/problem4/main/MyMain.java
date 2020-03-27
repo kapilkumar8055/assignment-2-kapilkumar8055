32f6d11c6af8daa0a31ee57294a0cee33e32d4a7
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-28 02:02:37 +0530

    create object of MyBinarySearchTree

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -1,8 +1,12 @@
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
     public static void main(String[] args) {
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
 
     }
 }
