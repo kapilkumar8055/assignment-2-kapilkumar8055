0010f3ee99b163bb6543f5831545929111c6930d
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-27 23:02:27 +0530

    count left nodes without child using method

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -8,14 +8,17 @@ public class MyMain {
     public static void main(String[] args) {
         MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree();
         TreeNode root = new TreeNode(20);
-        myBinarySearchTree.insert(root, 15);
         myBinarySearchTree.insert(root, 10);
-        myBinarySearchTree.insert(root, 12);
-        myBinarySearchTree.insert(root, 17);
-        myBinarySearchTree.insert(root, 25);
+        myBinarySearchTree.insert(root, 2);
         myBinarySearchTree.insert(root, 30);
-        myBinarySearchTree.insert(root, 5);
-        myBinarySearchTree.insert(root, 35);
-        myBinarySearchTree.insert(root, 32);
+        myBinarySearchTree.insert(root, 18);
+        myBinarySearchTree.insert(root, 14);
+        myBinarySearchTree.insert(root, 24);
+        myBinarySearchTree.insert(root, 60);
+        myBinarySearchTree.insert(root, 50);
+        myBinarySearchTree.insert(root, 80);
+        myBinarySearchTree.insert(root, 90);
+        myBinarySearchTree.insert(root, 12);
+        myBinarySearchTree.countNodesWithoutLeftChild(root);
     }
 }
