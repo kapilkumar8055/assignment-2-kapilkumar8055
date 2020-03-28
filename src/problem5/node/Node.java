053aa73669ac088d93b3c60d2f56235e8275c93a
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-28 16:51:20 +0530

    create variables

--- src/problem5/node/Node.java
+++ src/problem5/node/Node.java
@@ -1,10 +1,9 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.node;
+
+import problem5.student.Student;
+
 // to define node properties
 public class Node {
+    private Student student;
+    private Node next;
 }
