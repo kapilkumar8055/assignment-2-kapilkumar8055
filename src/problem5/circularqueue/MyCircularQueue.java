adadf8b25387aa84eb73a05e1e6f6d21898b3c14
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-28 16:57:13 +0530

    create getter and setter methods

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -7,4 +7,20 @@ public class MyCircularQueue {
     private Node front;
     private Node temp;
     private Node end;
+
+    public Node getFront() {
+        return front;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
+
+    public Node getTemp() {
+        return temp;
+    }
+
+    public void setTemp(Node temp) {
+        this.temp = temp;
+    }
 }
