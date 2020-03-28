57e32690831e5b95ca6bf7b53563c8b6b1508a4c
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-28 16:22:38 +0530

    creating getter and setter methods

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -10,4 +10,35 @@ public class Student {
     private int appearingCount;
     private Scanner scanner;
 
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public int getStudentRollNo() {
+        return studentRollNo;
+    }
+
+    public void setStudentRollNo(int studentRollNo) {
+        this.studentRollNo = studentRollNo;
+    }
+
+    public int getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(int backlog) {
+        this.backlog = backlog;
+    }
+
+    public int getAppearingCount() {
+        return appearingCount;
+    }
+
+    public void setAppearingCount(int appearingCount) {
+        this.appearingCount = appearingCount;
+    }
 }
