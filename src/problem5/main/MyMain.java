2b0860da700604a1a2bed7e96ad30bbc7f99b167
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-28 18:01:43 +0530

    entering the data

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -2,12 +2,16 @@ package problem5.main;
 
 import problem5.circularqueue.MyCircularQueue;
 import problem5.node.Node;
+import problem5.student.Student;
 
 //executable class
 public class MyMain {
     public static void main(String[] args) {
         MyCircularQueue myCircularQueue = new MyCircularQueue();
         Node node;
-
+        for (int i = 1; i < 10; i++) {
+            node = new Node(new Student());
+            myCircularQueue.enqueue(node);
+        }
     }
 }
