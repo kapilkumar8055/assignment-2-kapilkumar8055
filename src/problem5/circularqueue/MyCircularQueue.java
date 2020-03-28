c3533c97cc9861f5fb566079ee55f449d6e726a4
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-28 16:56:20 +0530

    create variables

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -1,10 +1,10 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.circularqueue;
+
+import problem5.node.Node;
+
 //to implement circular queue
 public class MyCircularQueue {
+    private Node front;
+    private Node temp;
+    private Node end;
 }
--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -1,9 +1,3 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.main;
 //executable class
 public class MyMain {
