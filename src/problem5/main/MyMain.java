8f3e1744a576486e8c26d8492492e5811db37bdc
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2020-03-28 17:59:31 +0530

    creating the object of MyCircularQueueClass

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -1,7 +1,13 @@
 package problem5.main;
+
+import problem5.circularqueue.MyCircularQueue;
+import problem5.node.Node;
+
 //executable class
 public class MyMain {
     public static void main(String[] args) {
+        MyCircularQueue myCircularQueue = new MyCircularQueue();
+        Node node;
 
     }
 }
